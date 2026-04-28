73a0fa2c1d2699a4481696b8baaa847860a7ab05
Z User <z@container>
2026-04-28 23:50:57 +0000

    f1edd412-2a3b-4c5a-ab85-ba4fd6413f90

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+exec &>> /home/z/my-project/dev.log
 while true; do
-  echo "$(date) Starting Next.js..."
+  echo "[$(date)] Starting Next.js..."
   cd /home/z/my-project
   npx next dev -p 3000 -H 0.0.0.0 2>&1
-  echo "$(date) Next.js died, restarting in 3s..."
-  sleep 3
+  EXIT_CODE=$?
+  echo "[$(date)] Next.js exited with code $EXIT_CODE, restarting in 2s..."
+  sleep 2
 done
